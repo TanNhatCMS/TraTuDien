3c9d55170e601bc2520b2d35eac99d85a57a2720
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2025-05-13 01:39:02 +0700

    chore settings to kotlin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,14 +3,20 @@ pluginManagement {
         gradlePluginPortal()
         google()
         mavenCentral()
+        maven { url = uri("https://plugins.gradle.org/m2/") }
+        maven { url = uri("https://jitpack.io") }
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://plugins.gradle.org/m2/") }
+        maven { url = uri("https://jitpack.io") }
     }
 }
+
 rootProject.name = "Tra Từ Điển"
-include ':app'
+include(":app")
